7f7a991e622b8af3367ceda79cde3d7e0c8ebbfb
JiHaD SeR <85656190+J-I-H-A-D@users.noreply.github.com>
2021-09-07 16:27:04 +0530

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -1,6 +1,5 @@
-/* Copyright (C) 2020 Yusuf Usta.
-recodded by afnanplk  
-PINKY V2
+/* Copyright © 2020 Yusuf Usta.
+recodded by Mikhaiel 
 */
 
 const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
