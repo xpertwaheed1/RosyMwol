b10858e9c96b19a56ced6ed2d3a70f9df5ccae2a
JiHaD SeR <85656190+J-I-H-A-D@users.noreply.github.com>
2021-09-07 22:15:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBU
 module.exports = {
     VERSION: 'v7.3.6',
     CHANNEL: 'https://t.me/remasterplugin',
-    SESSION: process.env.ElsaMowl_SESSION === undefined ? '' : process.env.ElsaMowl_SESSION,
+    SESSION: process.env.ElsaMwol_SESSION === undefined ? '' : process.env.ElsaMwol_SESSION,
     ANTİLİNK: process.env.ANTİ_LİNK === undefined ? 'false' : process.env.ANTİ_LİNK,
     AUTOBİO: process.env.AUTO_BİO === undefined ? 'false' : process.env.AUTO_BİO,
     THERI_KICK: process.env.THERI_KICK === undefined ? 'false' : process.env.THERI_KICK,
