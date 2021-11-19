a79b346fdcd02a61fc0803a08885ee171cb6bb9a
DEVILSER <88338865+DEVILSER@users.noreply.github.com>
2021-11-19 13:25:52 +0530

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -11,7 +11,7 @@ var ddd = ggg.toString('utf-8')
 
 let whb = Config.WORKTYPE == 'public' ? false : true
 
-WhatsAlexa.addCommand({pattern: 'Menu', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+WhatsAlexa.addCommand({pattern: 'Menu', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
 // send a buttons message!
     var plk_say = new Date().toLocaleString('HI', { timeZone: 'Asia/Kolkata' }).split(' ')[1]
 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -19,7 +19,8 @@ var plk_here = new Date().toLocaleDateString(get_localized_date)
 var afnplk = '```⏱ Time :' + plk_say + '```\n\n ```📅 Date :' + plk_here + '```'
 	const buttons = [
 
-        {buttonId: 'list', buttonText: {displayText: 'list'}, type: 1},
+        {buttonId: 'list', buttonText: {displayText: 'ʟɪꜱᴛ'}, type: 1},
+        {buttonId: 'git', buttonText: {displayText: 'ɢɪᴛ'}, type: 1},
 
       ]
       
