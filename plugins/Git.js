b8cccf2c513beb6e52d82ecec8eafe7d637754f0
DEVILSER <88338865+DEVILSER@users.noreply.github.com>
2021-11-20 23:03:52 +0530

    Create Git.js

---
+++ plugins/Git.js
@@ -0,0 +1,24 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'Git', fromMe: true, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://raw.githubusercontent.com/DEVILSER/DEVILSER/main/Media/Ammu/Git.jpg";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*SCAN THE QR FOR GIT*😉
+`}) 
+
+}));
