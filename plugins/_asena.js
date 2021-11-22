0848eee05ab2738b3f061d246638b1176e6faf88
devil-ser <89063822+devil-ser@users.noreply.github.com>
2021-11-22 19:31:16 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -12,7 +12,7 @@ const Lang = Language.getString('_asena');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'List ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+    Asena.addCommand({pattern: 'list ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
 
         var CMD_HELP = '';
         if (match[1] === '') {
@@ -120,7 +120,7 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-     Asena.addCommand({pattern: 'List ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+     Asena.addCommand({pattern: 'list ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
 
         var CMD_HELP = '';
         if (match[1] === '') {
